64112ee75bfc1a3bd99ca2e1597f7729f18dc100
ageu_dev19 <ageumenezes423@gmail.com>
2025-04-12 09:32:27 -0300

    Add update task feature

--- src/main/java/br/com/ageudev/todolist/task/TaskController.java
+++ src/main/java/br/com/ageudev/todolist/task/TaskController.java
@@ -14,7 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
@@ -51,5 +53,13 @@ public class TaskController {
     var idUser = request.getAttribute("idUser");
     var tasks = this.taskRepository.findByIdUser((UUID) idUser);
     return tasks;
+  }
+
+  @PutMapping("/{id}")
+  public TaskModel update(@RequestBody TaskModel taskModel, @PathVariable UUID id, HttpServletRequest request) {
+    var idUser = request.getAttribute("idUser");
+    taskModel.setIdUser((UUID) idUser);
+    taskModel.setId(id);
+    return this.taskRepository.save(taskModel);
   }
 }
